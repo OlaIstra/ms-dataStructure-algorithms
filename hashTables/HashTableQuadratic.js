245fbd0617c20fa97effb7641de224d7df20a5e9
OlaIstra <38282991+OlaIstra@users.noreply.github.com>
2020-02-24 14:09:43 +0100

    Update HashTableQuadratic.js

--- hashTables/HashTableQuadratic.js
+++ hashTables/HashTableQuadratic.js
@@ -5,7 +5,7 @@ function HashTableQuadratic(size) {
   this.values = this.initArr(size)
 }
 
-HashTableLinear.prototype.initArr = function(size) {
+HashTableQuadratic.prototype.initArr = function(size) {
  const arr = []
  for (i = 0; i < size; i++) {
    arr.push(null)
@@ -13,20 +13,20 @@ HashTableLinear.prototype.initArr = function(size) {
  return arr
 }
 
-HashTableLinear.prototype.hash = function(key) {
+HashTableQuadratic.prototype.hash = function(key) {
   return key % this.size
 }
 
-HashTableLinear.prototype.put = function(key, value) {  
+HashTableQuadratic.prototype.put = function(key, value) {  
 
   if (this.limit > this.size) throw "HashTable is full"
 
   const hashedIdx = this.hash(key)
-  let squareIdx = 1
+  let squareIndex = 1
 
   while (this.keys[hashedIdx] !== null) {
-    hashedIdx += Math.pow(squareIdx, 2)
-    squareIdx++
+    hashedIndex += Math.pow(squareIndex,2)
+    squareIndex++
   }
 
   this.keys[hashedIdx] = key
@@ -35,35 +35,18 @@ HashTableLinear.prototype.put = function(key, value) {
   this.limit++
 }
 
-HashTableLinear.prototype.put = function(key, value) {  
-
-  if (this.limit > this.size) throw "HashTable is full"
-
-  const hashedIdx = this.hash(key)
-
-  while (this.keys[hashedIdx] !== null) {
-    hashedIdx += Math.pow(squareIdx, 2)
-    hashedIdx = hashedIdx % this.size
-    squareIdx++    
-  }
-
-  this.keys[hashedIdx] = key
-  this.values[hashedIdx] = value
-
-  this.limit++
-}
-
-HashTableLinear.prototype.get = function(key) {
+HashTableQuadratic.prototype.get = function(key) {
   const hashedIdx = this.hash(key)
 
   while (this.keys[hashedIdx] !== key) {
-    hashedIdx += Math.pow(squareIdx, 2)
+    hashedIndex += Math.pow(squareIndex, 2)
     hashedIdx = hashedIdx % this.size
+    squareIndex++
   }
 
   return this.values[hashedIdx]
 }
 
-const hashOne = new HashTableQuadratic(11)
-hashOne.put(7, 'hi')
-console.log(hashOne.get(7))
+const hashQuadratic = new HashTableQuadratic(11)
+hashQuadratic.put(7, 'hi')
+console.log(hashQuadratic)
