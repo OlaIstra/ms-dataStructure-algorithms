544c9ffacf2be5ae6e62be8ffc6af39adfddf182
OlaIstra <38282991+OlaIstra@users.noreply.github.com>
2020-02-25 08:16:10 +0100

    Create sortableStackDesigning.js

---
+++ task__queue-stack/sortableStackDesigning.js
@@ -0,0 +1,26 @@
+function sortStack(size) {
+  this.size = size
+  this.initialStack = new Stack()
+  this.sortedStack = new Stack()
+
+  // let's initialize it with some random ints
+  for ( let i = 0; i < size; i++ ) {
+    this.initialStack.push(Math.floor(Math.random() * (size + 1)))
+  }
+}
+
+sortStack.prototype.sortDescending = function() {
+  while(!this.initialStack.isEmpty()) {
+    let temp = this.initialStack.delete()
+
+    while (!this.sortedStack.isEmpty() && this.sortedStack.peek() < temp){
+      this.initialStack.push(this.sortedStack.delete())
+    }
+    this.sortedStack.push(temp)
+  }
+}
+
+const stackToBeSorted = new sortStack(10)
+console.log(stackToBeSorted)
+stackToBeSorted.sortDescending()
+console.log(stackToBeSorted)
