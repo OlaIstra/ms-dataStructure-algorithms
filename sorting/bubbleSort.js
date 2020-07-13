04c5cd9671ef328f040c92e92215699f03ee3bc2
olga.ibragimova <38282991+OlaIstra@users.noreply.github.com>
2020-07-13 13:07:24 +0300

    Update bubbleSort.js

--- sorting/bubbleSort.js
+++ sorting/bubbleSort.js
@@ -19,3 +19,22 @@ bubbleSort([6,1,2,5,3,7,4])
 
 //Time Complexity: O(n2)
 //Space Complexity: O(1)
+
+
+function bubbleSort2(arr) {
+  let swapped = false
+    
+  do {
+    swapped = false
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > arr[i+1]) {
+        let temp = arr[i]
+        arr[i] = arr[i+1]
+        arr[i+1] = temp
+        swapped = true
+      }
+    }
+  } while (swapped)
+      
+  return arr
+}
