dbe14f00b2a26ff2a86da73fa56a9d5059628cf2
olga.ibragimova <38282991+OlaIstra@users.noreply.github.com>
2020-07-14 08:27:37 +0300

    Update insertionSort.js

--- sorting/insertionSort.js
+++ sorting/insertionSort.js
@@ -22,3 +22,15 @@ function insertionSort(arr) {
 insertionSort([6,1,23,4,2,3]); // [1, 2, 3, 4, 6, 23]
 // Time Complexity: O(n2)
 // Space Complexity: O(1)
+
+
+const insertionSort2 = (arr) => {
+ for (let i = 1; i < arr.length; i++) {
+  for (let j = 0; j < i; j++) {
+   if (arr[i] < arr[j]) {
+    const spliced = arr.splice(i, 1)
+    arr.splice(j, 0, spliced[0])
+   }
+  }
+ }
+}
