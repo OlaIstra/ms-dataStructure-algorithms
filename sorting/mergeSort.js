83b7e3eb9a476179b5b0aa928773c97236761cc4
olga.ibragimova <38282991+OlaIstra@users.noreply.github.com>
2020-07-15 09:24:14 +0300

    Update mergeSort.js

--- sorting/mergeSort.js
+++ sorting/mergeSort.js
@@ -29,3 +29,32 @@ function mergeSort(array) {
 console.log(mergeSort([6,1,23,4,2,3])) // [1, 2, 3, 4, 6, 23]
 //Time Complexity: O(nlog2(n))
 //Space Complexity: O(n)
+
+function merging(a1, a2) {
+  const result = [];
+
+  while (a1.length > 0 && a2.length > 0) {
+    if (a1[0] < a2[0]) {
+      result.push(a1.shift());
+    } else {
+      result.push(a2.shift());
+    }
+  }
+
+  return result.concat(a1, a2);
+}
+
+function mergeSort1(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+  const length = arr.length;
+  const middle = Math.floor(length / 2);
+
+  const left = arr.slice(0, middle);
+  const right = arr.slice(middle, length);
+
+  return merging(mergeSort(left), mergeSort(right));
+}
+
+console.log(mergeSort([3, 5, 2, 8, 1, 9, 2, 0]));
